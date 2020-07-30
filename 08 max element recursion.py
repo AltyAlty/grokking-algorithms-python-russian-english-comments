509390f8a59f1bbdafdf8bcad0f479edd482ef00
Alty <heneveraskedforthis@gmail.com>
2020-07-31 02:47:11 +0300

    fix the comments in the solution 7; refactor the names of the variables, fix russian comments and add english comments in the solution 8

--- 08 max element recursion.py
+++ 08 max element recursion.py
@@ -1,54 +1,166 @@
-# Создаем функцию для обнаружения наибольшего элемента в списке.
-def max(list):
-    # Определяем случай, если передан пустой массив, чтобы избежать ошибки.
-    if len(list) == 0:
+# Поиск наибольшего элемента используя рекурсию.
+# ----------
+# Finding the largest element using recursion.
+
+
+# Создаем функцию "find_max_one", которая принимает один входной параметр:
+# список "list_one", содержащий элементы для поиска наибольшего из них.
+# ----------
+# Create the function "find_max_one" that accepts one parameter:
+# the list "list_one" containing the elements to find the largest one.
+def find_max_one(list_one):
+    # Если список "list_two" содержит 0 элементов, то функция "find_max_one" возвращает "None".
+    # "None" означает nil (ничто) в Python. Используем, чтобы определять, что значения нет в списке.
+    # Ключевое слово "return" выходит из функции и возвращает какое-либо значение.
+    # ----------
+    # If the list "list_one" contains 0 elements, then the function "find_max_one" returns "None".
+    # "None" means nil (nothing) in Python. We use it to define that the number to find is not in the list.
+    # The keyword "return" is to exit a function and return a value.
+    if len(list_one) == 0:
         return None
-    # Определяем базовый случай как если массив содержит всего один элемент, то этот элемент будет являться наибольшим.
-    if len(list) == 1:
-        return list[0]
-
-    # Определяем рекурсивный случай.
-    # Если первый элемент списка больше второго элемента этого списка, то удаляем второй элемент в списке
-    # и рекурсивно вызываем функцию с новый урезанным списком.
-    if list[0] > list[1]:
-        del list[1]
-        return max(list)
-    # Иначе если первый элемент списка меньше или равен второму элементу этого списка,
-    # то рекурсивно вызываем функцию со новым урезанным списком без первого элемента при помощи среза.
+    # Создаем базовый случай.
+    # Базовый случай в рекурсивной функции - это часть кода функции, в которой описывается
+    # условие прекращения работы функции в целях предотвращения зацикливания.
+    # Если одна из вызванных рекурсивно копий функции "find_max_one" принимает в качестве параметра список,
+    # содержащий 1 элемент, то мы выходим из этой копии функции "find_max_one" и возвращаем первый элемент этого списка
+    # предыдущей копии функции "find_max_one" в стеке вызовов, поскольку если список содержит только один элемент,
+    # то этот элемент будет наибольшим.
+    # Если изначально вызывается функция "find_max_one", которая принимает в качестве параметра список,
+    # содержащий 1 элемент, то функция "find_max_one" сразу возвращает первый элемент этого списка,
+    # поскольку если список содержит только один элемент, то этот элемент будет наибольшим.
+    # ----------
+    # Create the base case.
+    # The base case in a recursive function is a part of the function code that describes
+    # the condition for the termination of the function in order to prevent loops.
+    # If one of the recursively called copies of the function "find_max_one" takes the list containing 1 element
+    # as a parameter, then we exit this recursively called copy of the function "find_max_one"
+    # and it returns the first element of this list to the previous copy of the function "find_max_one"
+    # in the call stack, since if a list contains only 1 element, then this element is the largest one.
+    # If the function "find_max_one" is initially called with the list containing 1 element as a parameter,
+    # then the function "find_max_one" immediately returns the first element of this list,
+    # since if a list contains only 1 element, then this element is the largest one.
+    if len(list_one) == 1:
+        return list_one[0]
+    # Создаем рекурсивный случай.
+    # Рекурсивный случай в рекурсивной функции - это часть кода функции, в которой функция вызывает сама себя
+    # в целях выполнения какой-либо задачи.
+    # Если первый элемент больше второго элемента в списке "list_one", то этот второй элемент удаляется
+    # при помощи ключевого слова "del" и рекурсивно вызывается новая копия функции "find_max_one"
+    # с этим уменьшенным списком "list_one" в качестве параметра.
+    # Когда рекурсивно вызывается копия функции "find_max_one", которая принимает в качестве параметра список,
+    # содержащий 1 элемент, то срабатывает базовый случай.
+    # Копии функции "find_max_one" в стеке вызовов поочередно завершают свою работу
+    # и возвращают свои рассчитанные значения предыдущим рекурсивно вызванным копиям функции "find_max_one" до тех пор,
+    # пока не завершит работу самая первая вызванная функция "find_max_one".
+    # ----------
+    # Create the recursive case.
+    # The recursive case in a recursive function is a part of the function code
+    # in which the function calls itself in order to perform a task.
+    # If the first element is bigger than the second element in the list "list_one", then this second element is deleted
+    # using the keyword "del" and the new copy of the function "find_max_one" is recursively called
+    # with this reduced list "list_one" as a parameter.
+    # When a copy of the function "find_max_one" is recursively called with the list containing 1 element
+    # as a parameter, then the base case is triggered.
+    # The copies of the function "find_max_one" in the call stack terminate one by one and return
+    # their calculated values to the previous recursively called copies of the function "find_max_one"
+    # until the very first called function "find_max_one" exits.
+    if list_one[0] > list_one[1]:
+        del list_one[1]
+        return find_max_one(list_one)
+    # Иначе если первый элемент меньше второго элемента в списке "list_one", то рекурсивно вызывается
+    # новая копия функции "find_max_one" со списком "list_one", за исключением первого элемента, в качестве параметра.
+    # ----------
+    # Otherwise, if the first element is less than the second element in the list "list_one",
+    # then the new copy of the function "find_max_one" is recursively called with the list "list_one"
+    # except for the first element as a parameter.
     else:
-        return max(list[1:])
+        return find_max_one(list_one[1:])
 
 
-mylist1 = [2, 7, 45, 3, 5]
-mylist2 = []
-print(max(mylist1))
-print(max(mylist2))
+# Создаем 2 списка и попытаемся найти в них наибольшие элементы.
+# Список "my_list1" содержит несколько чисел, а список "my_list2" является пустым.
+# Функция "print()" выводит некую указанную информацию на экран или на какое-либо другое устройство вывода.
+# ----------
+# Create the 2 lists and try to find the largest elements in these lists.
+# The list "my_list1" contains several numbers, but the list "my_list2" is empty.
+# The function "print()" prints the specified message to the screen, or other standard output device.
+my_list1 = [2, 0, 6, 9, 7, 7]
+my_list2 = []
+print(find_max_one(my_list1))
+print(find_max_one(my_list2))
 
 
-# Создаем еще один вариант функции для обнаружения наибольшего элемента в списке.
-def max_(list):
-    # Определяем случай, если передан пустой массив, чтобы избежать ошибки.
-    if len(list) == 0:
+# Давайте создадим еще один вариант функции для поиска наибольшего элемента.
+# Здесь мы создаем другой рекурсивный случай.
+# Создаем функцию "find_max_two", которая принимает один входной параметр:
+# список "list_two", содержащий элементы для поиска наибольшего из них.
+# ----------
+# Let's create another variation of the function to find the largest element.
+# Here we are creating another recursive case.
+# Create the function "find_max_two" that accepts one parameter:
+# the list "list_two" containing the elements to find the largest one.
+def find_max_two(list_two):
+    # Если список содержит 0 элементов, то функция "find_max_two" возвращает "None".
+    # "None" означает nil (ничто) в Python. Используем, чтобы определять, что значения нет в списке.
+    # Ключевое слово "return" выходит из функции и возвращает какое-либо значение.
+    # ----------
+    # If the list "list_two" contains 0 elements, then the function "find_max_two" returns "None".
+    # "None" means nil (nothing) in Python. We use it to define that the number to find is not in the list.
+    # The keyword "return" is to exit a function and return a value.
+    if len(list_two) == 0:
         return None
-    # Определяем базовый случай как если массив содержит всего один элемент, то этот элемент будет являться наибольшим.
-    if len(list) == 1:
-        return list[0]
-
-    # Определяем рекурсивный случай.
-
+    # Создаем базовый случай.
+    # Если одна из вызванных рекурсивно копий функции "find_max_two" принимает в качестве параметра список,
+    # содержащий 1 элемент, то мы выходим из этой копии функции "find_max_two" и возвращаем первый элемент этого списка
+    # предыдущей копии функции "find_max_two" в стеке вызовов, поскольку если список содержит только один элемент,
+    # то этот элемент будет наибольшим.
+    # Если изначально вызывается функция "find_max_two", которая принимает в качестве параметра список,
+    # содержащий 1 элемент, то функция "find_max_two" сразу возвращает первый элемент этого списка,
+    # поскольку если список содержит только один элемент, то этот элемент будет наибольшим.
+    # ----------
+    # Create the base case.
+    # If one of the recursively called copies of the function "find_max_two" takes the list containing 1 element
+    # as a parameter, then we exit this recursively called copy of the function "find_max_two"
+    # and it returns the first element of this list to the previous copy of the function "find_max_two"
+    # in the call stack, since if a list contains only 1 element, then this element is the largest one.
+    # If the function "find_max_two" is initially called with the list containing 1 element as a parameter,
+    # then the function "find_max_two" immediately returns the first element of this list,
+    # since if a list contains only 1 element, then this element is the largest one.
+    if len(list_two) == 1:
+        return list_two[0]
+    # Создаем рекурсивный случай.
+    # Рекурсивно вызывается новая копия функции "find_max_two" со списком "list_two", за исключением первого элемента,
+    # в качестве параметра, причем результат работы этой рекурсивно вызванной копии функции "find_max_two" хранится
+    # в локальной переменной "sub_max".
+    # Любая копия функции "find_max_two" возвращает результат на основе следующего условия:
+    # она возвращает первый элемент списка "list_two", если этот первый элемент больше
+    # значения локальной переменной "sub_max", иначе возвращается эта локальная переменная "sub_max".
+    # Когда рекурсивно вызывается копия функции "find_max_two", которая принимает в качестве параметра список,
+    # содержащий 1 элемент, то срабатывает базовый случай.
+    # Копии функции "find_max_two" в стеке вызовов поочередно завершают свою работу
+    # и возвращают свои рассчитанные значения предыдущим рекурсивно вызванным копиям функции "find_max_two" до тех пор,
+    # пока не завершит работу самая первая вызванная функция "find_max_two".
+    # ----------
+    # Create the recursive case.
+    # The new copy of the function "find_max_two" is recursively called with the list "list_two"
+    # except for the first element as a parameter,
+    # and the result of this recursively called copy of the function "find_max_two" is stored
+    # in the local variable "sub_max".
+    # Any copy of the function "find_max_two" returns a result based on the following condition:
+    # it returns the first element of the list "list_two" if this first element is bigger than the value of
+    # the local variable "sub_max", otherwise this local variable "sub_max" is returned.
+    # When a copy of the function "find_max_two" is recursively called with the list containing 1 element
+    # as a parameter, then the base case is triggered.
+    # The copies of the function "find_max_two" in the call stack terminate one by one and return
+    # their calculated values to the previous recursively called copies of the function "find_max_two"
+    # until the very first called function "find_max_two" exits.
     else:
-        # Переменная sub_max содержит результат рекурсивно вызванной функции со списком без первого элемента.
-        sub_max = max_(list[1:])
-        # [5, 2, 1, 4] - [2, 1, 4]
-        # [2, 1, 4] - [1, 4]
-        # [1, 4] - [4]
-        # [4]
-
-        # Вовзращаем первый элемент текущего списка, если этот элемент больше значения,
-        # полученного из следующей рекурсивно вызваннной функции.
-        # Иначе возвращаем значение, полученное из следующей рекурсивно вызваннной функции.
-        return list[0] if list[0] > sub_max else sub_max
+        sub_max = find_max_two(list_two[1:])
+        return list_two[0] if list_two[0] > sub_max else sub_max
 
 
-mylist3 = [5, 2, 1, 4]
-print(max_(mylist3))
+# Попытаемся найти наибольшие элементы в списках "my_list1" и "my_list2", используя функцию "find_max_two".
+# ----------
+# Try to find the largest elements in the lists "my_list1" and "my_list2" using the function "find_max_two".
+print(find_max_two(my_list1))
+print(find_max_two(my_list2))
